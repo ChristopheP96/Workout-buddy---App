1cb20eb31f6fe6c76265b8492d20533ba9056c25
iondo <ines.ondo.baka@gmail.com>
2019-02-27 18:09:22 +0100

    add session packages

--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const profileRouter = require('./routes/profile');
+
+mongoose.connect('mongodb://localhost:27017/WorkoutApp', { useNewUrlParser: true })
+  .then(() => {
+    console.log('connected');
+  })
+  .catch((error) => {
+    next(error);
+  });
 
 const app = express();
 
@@ -22,6 +33,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'WorkoutApp',
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60, // 1 day
+  }),
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
