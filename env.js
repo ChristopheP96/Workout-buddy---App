7d6923c6ad507d63f8fd9de0f18951ba649c9636
iondo <ines.ondo.baka@gmail.com>
2019-03-10 21:26:02 +0100

    Fix env.js

--- env.js
+++ env.js
@@ -1,23 +1,15 @@
+/* eslint-disable func-names */
 module.exports = function (app, express) {
-  app.configure(() => {
-    app.use(express.logger());
-    app.use(express.static(path.join((__dirname + '/public'));
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs'); //extension of views
-
-  });
-
-  //development configuration
-  app.configure('development', () => {
-    app.use(express.errorHandler({
-      dumpExceptions: true,
-      showStack: true
-    }));
-  });
-
-  //production configuration
-  app.configure('production', () => {
-    app.use(express.errorHandler());
-  });
-
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true,
+      }));
+      break;
+    case 'production':
+      app.use(express.errorHandler());
+      break;
+    default:
+  }
 };
